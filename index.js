62ac5a2d674aa7e63c455a8051a92fb533c6f78e
Cole Vanacore <colevanacore@gmail.com>
2018-09-10 16:35:52 -0600

    Add search ideas function and contenteditable

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ var numCards = 0;
 //this function is creating a new card using the parameters 
 function generateHTMLCard(card) {
   return `<div id="${card.id}" class="card-container">
-            <p class = "title-of-card"> ${card.title}<button class="delete-button"></button> </p>
-            <p class="body-of-card"> ${card.body} </p> 
+            <p class = "title-of-card" contenteditable=true> ${card.title}<button class="delete-button"></button> </p>
+            <p class="body-of-card" contenteditable=true> ${card.body} </p> 
             <p class="card-buttons"> 
                 <button class="upvote"></button> <button class="downvote"></button> 
                 <span class="quality"> quality: <span class="qualityVariable"> ${card.quality} </span> </span>
@@ -57,6 +57,7 @@ function cardChanges(event) {
 
 function deleteCard() {
   $(event.target).closest('.card-container').remove();
+  deleteFromLocalStorage();
 }
 
 function changeQualityUp() {
@@ -88,6 +89,10 @@ function updateStoredQuality(event){
   localStorage.setItem(timeStamp, stringifiedStoredIdea);
 }
 
+function deleteFromLocalStorage(){
+   var timeStamp = $(event.target).closest('.card-container').attr('id');
+   localStorage.removeItem(timeStamp);
+}
 
 function Card (id, title, body, quality){
   this.title = title;
@@ -115,88 +120,22 @@ function getIdeaFromLocalStorage(){
   }
 };
 
+$('.search-input').on('keyup', searchIdeas);
 
-
-// $(".bottom-box").on('click', changeQuality);
-
-// function changeQuality(event){
-//   if (event.target.className === "upvote"){
-//   upVoteIdea(event);
-//   }
-//   else if (event.target.className === "downvote")
-//     downVoteIdea(event);
-// };
-
-// function upVoteIdea(event){
-//   var currentQuality = $($(event.target).siblings('span').children()[0]).text().trim();
-//   var qualityVariable = "swill";
-//   // var newText = $($(event.target).siblings('p.quality').children()[0]);
-//       if (currentQuality.text().trim() === "swill") {
-//         console.log('hi');
-//             qualityVariable = "plausible";
-//         // debugger
-//             currentQuality.text('plausible');
-//           }
-//    else if (currentQuality === "plausible"){
-//       qualityVariable = "genius";
-//       nextText.text(qualityVariable);
-//   }
-// };
-
-// function downVoteIdea(event){
-//   console.log('hey');
-// }
-
-
-
-// $(".bottom-box").on('click', function(event){
-//     var currentQuality = $($(event.target).siblings('p.quality').children()[0]).text().trim();
-//     var qualityVariable = "swill";
-
-//     if (event.target.className === "upvote" || event.target.className === "downvote"){
-
-//         if (event.target.className === "upvote" && currentQuality === "plausible"){
-//             qualityVariable = "genius";
-//             $($(event.target).siblings('p.quality').children()[0]).text(qualityVariable);
-               
-//         } else if (event.target.className === "upvote" && currentQuality === "swill") {
-//             qualityVariable = "plausible";
-//             $($(event.target).siblings('p.quality').children()[0]).text(qualityVariable);
-               
-//         } else if (event.target.className === "downvote" && currentQuality === "plausible") {
-//             qualityVariable = "swill"
-//             $($(event.target).siblings('p.quality').children()[0]).text(qualityVariable);
-
-//         } else if (event.target.className === "downvote" && currentQuality === "genius") {
-//             qualityVariable = "plausible"
-//             $($(event.target).siblings('p.quality').children()[0]).text(qualityVariable);
-
-//         } else if (event.target.className === "downvote" && currentQuality === "swill") {
-//             qualityVariable = "swill";
-        
-//         } else if (event.target.className === "upvote" && currentQuality === "genius") {
-//             qualityVariable = "genius";
-//         }
-
-//     var cardHTML = $(event.target).closest('.card-container');
-//     var cardHTMLId = cardHTML[0].id;
-//     var cardObjectInJSON = localStorage.getItem(cardHTMLId);
-//     var cardObjectInJS = JSON.parse(cardObjectInJSON);
-
-//     cardObjectInJS.quality = qualityVariable;
-
-//     var newCardJSON = JSON.stringify(cardObjectInJS);
-//     localStorage.setItem(cardHTMLId, newCardJSON);
-//     }
-   
-//     else if (event.target.className === "delete-button") {
-//         var cardHTML = $(event.target).closest('.card-container').remove();
-//         var cardHTMLId = cardHTML[0].id;
-//         localStorage.removeItem(cardHTMLId);
-//     }
-// });
-
-
+function searchIdeas(){
+  var searchInput = $('.search-input').val().toLowerCase();
+  var allIdeas = $('.card-container');
+  for (var i=0; i < allIdeas.length; i++) {
+    var ideaTitle = $(allIdeas[i]).children('.title-of-card').text().toLowerCase();
+    var ideaBody = $(allIdeas[i]).children('.body-of-card').text().toLowerCase();
+      if (ideaTitle.includes(searchInput) || ideaBody.includes(searchInput)){
+        $(allIdeas[i]).removeClass('hidden');
+      }
+      else {
+      $(allIdeas[i]).addClass('hidden');
+      }
+    }
+  }
 
       
 
