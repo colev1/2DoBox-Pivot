43f95d21ecfff8d7fa37cc4c823c33cba8908362
Paul Vangelakos <paulvangelakos@gmail.com>
2018-09-12 16:15:20 -0600

    fixed the styling and added some CSS for mobile

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 $(document).ready(getTaskFromLocalStorage);
 
-
-//this function is creating a new card using the parameters 
 function generateHTMLCard(card) {
   return `<div id="${card.id}" class="card-container">
             <p class = "title-of-card" contenteditable=true onfocusout=updateTaskTitle(event)> ${card.title}<button class="delete-button"></button> </p>
@@ -12,22 +10,39 @@ function generateHTMLCard(card) {
             <button class="completed-task-button">Completed Task</button></p>`
 };
 
+function submitTask(event){
+  event.preventDefault();
+  var timeStamp = Date.now();
+  var card = new Card(timeStamp, $('.title-input').val(), $('.task-input').val());
+  var cardHTML = generateHTMLCard(card);
+  $( ".bottom-box" ).prepend(cardHTML);   
+  localStoreCard(card);
+  resetForm();
+};
+
+function Card (id, title, body, importance){
+  this.title = title;
+  this.body = body;
+  this.importance = "normal";
+  this.id = id;
+  this.completedTask = false;
+  this.hidden = false;
+  return this;
+};
+
 $('.save-btn').on('click', submitTask); 
-
 $('.task-input').on('keyup', enableSubmitButton);
-
 $('.show-all-completed').on('click', showCompletedTodos)
-
-
+$('.bottom-box').on('click', cardChanges)
+$('.filter-input').on('keyup', filterTasks);
 
 function enableSubmitButton(){
   if ($('.title-input').val() === "" || $('.task-input').val() === ""){
    $('.save-btn').attr('disabled', true);
   } else {
     $('.save-btn').attr('disabled', false);
-  }
-}
-
+  };
+};
 
 function completedTask(event) {
   var completedTaskCard = $(event.target).closest('.card-container');
@@ -37,26 +52,18 @@ function completedTask(event) {
   storedTask.completedTask = !storedTask.completedTask;
   storedTask.hidden = !storedTask.hidden;
   localStoreCard(storedTask);
-}
-
-
-function submitTask(event){
-  event.preventDefault();
-  var timeStamp = Date.now();
-  var card = new Card(timeStamp, $('.title-input').val(), $('.task-input').val());
-  var cardHTML = generateHTMLCard(card);
-  $( ".bottom-box" ).prepend(cardHTML);   
-  localStoreCard(card);
-  resetForm();
-}
+};
 
 function resetForm() {
   $('.title-input').val('');
   $('.task-input').val('');
   enableSubmitButton();
-}
+};
 
-$('.bottom-box').on('click', cardChanges)
+function deleteCard() {
+  $(event.target).closest('.card-container').remove();
+  deleteFromLocalStorage();
+}
 
 function cardChanges(event) {
   if (event.target.className === "delete-button") {
@@ -70,13 +77,7 @@ function cardChanges(event) {
   } if (event.target.className === "completed-task-button") {
     completedTask(event);
   }
-}
-
-function deleteCard() {
-  $(event.target).closest('.card-container').remove();
-  deleteFromLocalStorage();
-}
-
+};
 
 function upVoteImportance(event){
   var $currentimportance = $($(event.target).siblings('.importance').children()[0]).text().trim();
@@ -86,18 +87,17 @@ function upVoteImportance(event){
       return $currentimportance = arr[i+1];
     }
   }
-}
-
+};
 
 function downVoteImportance(event){
   var $currentimportance = $($(event.target).siblings('.importance').children()[0]).text().trim();
-    var arr = ["none", "low", "normal", "high", "critical"];
+  var arr = ["none", "low", "normal", "high", "critical"];
     for (var i =0; i < arr.length; i++) {
-      if ($currentimportance === arr[i]){
+      if ($currentimportance === arr[i]) {
         return $currentimportance = arr[i-1];
-      }
     }
-}
+  }
+};
 
 function updateStoredimportance(event) {
   var timeStamp = $(event.target).closest('.card-container').attr('id');
@@ -106,45 +106,29 @@ function updateStoredimportance(event) {
   storedTask.importance = $currentimportance.text();
   var stringifiedstoredTask = JSON.stringify(storedTask);
   localStorage.setItem(timeStamp, stringifiedstoredTask);
-}
+};
 
 function deleteFromLocalStorage(){
    var timeStamp = $(event.target).closest('.card-container').attr('id');
    localStorage.removeItem(timeStamp);
-}
-
-function Card (id, title, body, importance){
-  this.title = title;
-  this.body = body;
-  this.importance = "normal";
-  this.id = id;
-  this.completedTask = false;
-  this.hidden = false;
-  return this;
-}
-
-
+};
 
 function localStoreCard(card) {
     var cardString = JSON.stringify(card);
     localStorage.setItem(card.id, cardString);
-}
-
+};
 
 function getTaskFromLocalStorage(){
   for (var i=0; i<localStorage.length; i++) {
     var timeStamp = localStorage.key(i);
     var stringifiedTask = localStorage.getItem(timeStamp)
     var parsedTaskToDisplay= JSON.parse(stringifiedTask);
-     var cardHTML = generateHTMLCard(parsedTaskToDisplay);
-    if (parsedTaskToDisplay.completedTask === false){    
-    $( ".bottom-box" ).prepend(cardHTML);   
+    var cardHTML = generateHTMLCard(parsedTaskToDisplay);
+    if (parsedTaskToDisplay.completedTask === false) {    
+      $( ".bottom-box" ).prepend(cardHTML);   
     }
   }
 };
-
-
-$('.filter-input').on('keyup', filterTasks);
 
 function filterTasks(){
   var filterInput = $('.filter-input').val().toLowerCase();
@@ -152,14 +136,13 @@ function filterTasks(){
   for (var i=0; i < allTasks.length; i++) {
     var taskTitle = $(allTasks[i]).children('.title-of-card').text().toLowerCase();
     var taskBody = $(allTasks[i]).children('.task-of-card').text().toLowerCase();
-      if (taskTitle.includes(filterInput) || taskBody.includes(filterInput)){
-        $(allTasks[i]).removeClass('hidden');
-      }
-      else {
+    if (taskTitle.includes(filterInput) || taskBody.includes(filterInput)){
+      $(allTasks[i]).removeClass('hidden');
+    } else {
       $(allTasks[i]).addClass('hidden');
-      }
     }
   }
+};
 
 function updateTaskTitle(event){
   var currentTimeStamp = $(event.target).closest('.card-container').attr('id');
@@ -181,7 +164,6 @@ function updateTaskBody(event){
 
 function showCompletedTodos(event) {
   event.preventDefault();
-  console.log('hi');
   for (var i=0; i<localStorage.length; i++) {
     var timeStamp = localStorage.key(i);
     var stringifiedTask = localStorage.getItem(timeStamp);
